c5a5a8039fa9cf9785868d4f128315a13000726c
hahuydz <haquanghuy2006abc@gmail.com>
2025-07-13 23:26:53 +0700

    b6.c

---
+++ b6.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+}Node;
+
+typedef struct Queue {
+    struct Node* front;
+    struct Node* rear;
+}Queue;
+
+Node* createNode() {
+    int data;
+    printf("Enter data: ");
+    scanf("%d", &data);
+
+    Node* node = ( Node* )malloc(sizeof( Node ));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+Queue* createQueue() {
+    Queue* queue = (Queue*)malloc(sizeof( Queue ));
+    queue->front = NULL;
+    queue->rear = NULL;
+    return queue;
+}
+
+int isEmpty(Queue* queue) {
+    return queue->front == NULL;
+}
+
+int push(Queue* queue) {
+    Node* newNode = createNode();
+    if ( queue->front == NULL ) {
+        queue->front = queue->rear = newNode;
+    }else {
+        queue->rear->next = newNode;
+        queue->rear = queue->rear->next;
+    }
+    return newNode->data;
+}
+
+void show(Queue* queue) {
+    if ( isEmpty(queue) ) {
+        printf("Queue is empty\n");
+        return;
+    }
+    Node* node = queue->front;
+    printf("List: \n");
+    while ( node != NULL ) {
+        printf("%d\n", node->data);
+        node = node->next;
+    }
+}
+
+int main(void) {
+    Queue *queue = createQueue();
+    for ( int i = 1; i <= 5; i++ ) {
+        push(queue);
+    }
+    show(queue);
+    return 0;
+}
